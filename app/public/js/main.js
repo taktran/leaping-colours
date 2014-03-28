d1194fb7bdd8f69dd537732b56327eb25f91b4ff
Tak Tran <contact@tutaktran.com>
2014-03-28 17:44:36 +0000

    Add leap colour change.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,4 +1,4 @@
-/*global Two:true */
+/*global Two:true, Leap:true */
 (function (){
   'use strict';
 
@@ -18,12 +18,13 @@
     }).appendTo(document.body);
 
     var rect = two.makeRectangle(two.width / 2, two.height / 2, 250, 250);
+    var rectElem = $('#two-' + rect.id);
     rect.noStroke().fill = getRandomColor();
 
     // Update the renderer in order to generate corresponding DOM Elements.
     two.update();
 
-    $(('#two-' + rect.id))
+    rectElem
       .css('cursor', 'pointer')
       .click(function(e) {
         rect.fill = getRandomColor();
@@ -31,6 +32,26 @@
 
     two.bind('update', function(frameCount, timeDelta) {
       rect.rotation = frameCount / 60;
+    });
+
+
+    Leap.loop(function(obj) {
+      if (obj.hands.length < 1) {
+        return;
+      }
+
+      var hand = obj.hands[0];
+
+      var x = hand.palmPosition[0];
+      var y = hand.palmPosition[1];
+      // var z = hand.palmPosition[2];
+
+      var hue = Math.round(x/2) % 360;
+      var saturation = Math.round(y/3);
+
+      var color = "hsl(" + hue + "," + saturation + "%,50%)";
+      rect.fill = color;
+
     });
 
   });
